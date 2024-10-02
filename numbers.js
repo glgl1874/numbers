f87788a8578d216e3189f948c66c69e8d592614c
김정훈 <glgl1874@naver.com>
2024-10-02 21:59:43 +0900

    All

--- numbers.js
+++ numbers.js
@@ -2,8 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
+});
 
-app.use(cors());
+app.use(cors({
+    origin: '*',  // 모든 출처를 허용하거나 특정 출처만 허용할 수 있습니다.
+    methods: ['GET', 'POST'],
+  }));
 
 // form_data.json 파일을 클라이언트에게 제공
 app.get('/data', (req, res) => {
