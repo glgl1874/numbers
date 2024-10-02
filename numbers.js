df92bc2eb6eaaced3d367c084c4cdc3f33eccd04
김정훈 <glgl1874@naver.com>
2024-10-02 22:03:23 +0900

    All

--- numbers.js
+++ numbers.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
+
+app.listen(4000, () => {
+    console.log(`서버가 4000번 포트에서 실행 중입니다.`);
 });
 
 app.use(cors({
